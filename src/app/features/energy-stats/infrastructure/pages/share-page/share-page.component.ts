c4c6c24773ecd9f692320cb750c3fd4e0e24fe99
alex <alexlopezpal96@gmail.com>
2024-07-03 15:28:01 +0200

    added: fake share-page

--- src/app/features/energy-stats/infrastructure/pages/share-page/share-page.component.ts
+++ src/app/features/energy-stats/infrastructure/pages/share-page/share-page.component.ts
@@ -4,6 +4,11 @@ import {EnergyPrice, SharingUsersService} from "../../services/sharing-users.ser
 import {MonitoringService} from "../../services/monitoring.service";
 import {AddUserFormModalComponent} from "../../components/add-user-form-modal/add-user-form-modal.component";
 import {NavbarComponent} from "../../../../../shared/infrastructure/components/navbar/navbar.component";
+import {TranslocoDirective} from "@jsverse/transloco";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CalendarModule} from "primeng/calendar";
+import dayjs from "dayjs";
+import {NgForOf, NgIf} from "@angular/common";
 
 
 @Component({
@@ -12,6 +17,12 @@ import {NavbarComponent} from "../../../../../shared/infrastructure/components/n
   imports: [
     NgbModule,
     NavbarComponent,
+    TranslocoDirective,
+    ReactiveFormsModule,
+    CalendarModule,
+    FormsModule,
+    NgIf,
+    NgForOf,
   ],
   providers: [
     MonitoringService,
@@ -21,6 +32,38 @@ import {NavbarComponent} from "../../../../../shared/infrastructure/components/n
 })
 export class SharePageComponent {
 
+  fromDate: Date = dayjs().subtract(1).toDate();
+  toDate: Date = dayjs().toDate();
+  maxDate: Date = dayjs().toDate();
+
+  loading: boolean = false;
+
+  sharedData = [
+    {
+      date: '16/06/24 00:00',
+      type: "buy",
+      qty: "20",
+      total: "10",
+      before: "20",
+      now: "17"
+    },
+    {
+      date: '1/06/24 15:00',
+      type: "sell",
+      qty: "30",
+      total: "19",
+      before: "80",
+      now: "71"
+    },
+    {
+      date: '11/06/24 20:00',
+      type: "sell",
+      qty: "13",
+      total: "40",
+      before: "53",
+      now: "40"
+    }
+  ]
   constructor(
     private ngModal: NgbModal,
     protected sharingUsers: SharingUsersService,
@@ -28,12 +71,9 @@ export class SharePageComponent {
   ) {
   }
 
-  addUser() {
-    this.ngModal.open(AddUserFormModalComponent);
-  }
 
-  removeUser(id: string) {
-    this.sharingUsers.removeUser(id);
+  getData(){
+    console.log('DATA')
   }
 
   protected readonly EnergyPrice = EnergyPrice;
