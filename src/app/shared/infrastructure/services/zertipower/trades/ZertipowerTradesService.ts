d1b9665d5c556e27e439c57dc6a5d3febf61e0f5
alex <alexlopezpal96@gmail.com>
2024-09-05 11:40:18 +0200

    updated: share mobile design

--- src/app/shared/infrastructure/services/zertipower/trades/ZertipowerTradesService.ts
+++ src/app/shared/infrastructure/services/zertipower/trades/ZertipowerTradesService.ts
@@ -18,54 +18,9 @@ export class ZertipowerTradesService{
   }
 
   async getTradesByCustomer(customerId: number, fromDate: string, toDate: string): Promise<TradeInterface[]> {
-    /*const response = await this.axios.get<HttpResponse<TradeInterface[]>>(`/trades/customer/${customerId}/date/${fromDate}/${toDate}`);
-    return response.data.data;*/
-    return [
-      {
-        "id": 1,
-        "action": "BUY",
-        "tradedKwh": 0.017000,
-        "cost": 0.002000,
-        "previousKwh": 0.333000,
-        "currentKwh": 0.316000,
-        "infoDt": "2024-08-11 19:00:00",
-        "fromWalletAddress": "",
-        "toWalletAddress": "0x2FC7a05b837956985f7f6Bd1513bdD21635e3956"
-      },
-      {
-        "id": 7,
-        "action": "BUY",
-        "tradedKwh": 0.017000,
-        "cost": 0.002000,
-        "previousKwh": 0.142000,
-        "currentKwh": 0.125000,
-        "infoDt": "2024-08-11 19:00:00",
-        "fromWalletAddress": "",
-        "toWalletAddress": "0x2FC7a05b837956985f7f6Bd1513bdD21635e3956"
-      },
-      {
-        "id": 8,
-        "action": "SELL",
-        "tradedKwh": 0.017000,
-        "cost": 0.002000,
-        "previousKwh": 0.119000,
-        "currentKwh": 0.102000,
-        "infoDt": "2024-08-11 19:00:00",
-        "fromWalletAddress": "0x2FC7a05b837956985f7f6Bd1513bdD21635e3956",
-        "toWalletAddress": ''
-      },
-      {
-        "id": 9,
-        "action": "BUY",
-        "tradedKwh": 0.017000,
-        "cost": 0.002000,
-        "previousKwh": 0.131000,
-        "currentKwh": 0.114000,
-        "infoDt": "2024-08-11 19:00:00",
-        "fromWalletAddress": '',
-        "toWalletAddress": "0x2FC7a05b837956985f7f6Bd1513bdD21635e3956"
-      }
-    ];
+    const response = await this.axios.get<HttpResponse<TradeInterface[]>>(`/trades/customer/${customerId}/date/${fromDate}/${toDate}`);
+    return response.data.data;
+
   }
 
   async getTradesByCommunity(communityId: number, fromDate: string, toDate: string): Promise<TradeInterface[]> {
